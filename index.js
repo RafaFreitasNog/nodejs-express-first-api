be3c7bf797d081e35ebf6fbd444fce115e3771fa
Raphael Freitas Nogueira de Souza <raphaelfreitas18.dev@gmail.com>
2022-09-14 18:14:00 -0300

    Installed CORS and used it in index.js

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const articlesRouter = require('./src/routes/articles')
 const columnistsRouter = require('./src/routes/columnists')
 const usersRouter = require('./src/routes/users')
 require('./config/database')
+const cors = require('cors')
 
 app.listen('3000')
 
 // midlewares
     // transforms everything into JSON format
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
+
     // uses the routers
-app.use('/articles', articlesRouter)
-app.use('/columnists', columnistsRouter)
-app.use('/users', usersRouter)
+app.use('/articles', articlesRouter);
+app.use('/columnists', columnistsRouter);
+app.use('/users', usersRouter);
